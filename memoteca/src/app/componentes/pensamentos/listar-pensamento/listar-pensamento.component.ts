27312fe95f2673aca46847184144f4d111d42e72
welligton-home <welligtoncos@gmail.com>
2023-03-26 11:46:58 -0300

    Botão para carregar mais dados

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,6 +11,7 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) { }
 
@@ -20,6 +21,14 @@ export class ListarPensamentoComponent implements OnInit {
     })
   }
 
-
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual)
+    .subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(this.listaPensamentos.length){
+        this.haMaisPensamentos = false;
+      }
+    })
+  }
 
 }
